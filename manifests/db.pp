4062612ecefedc954260dfb816fcc82aecef6910
James Paton-Smith <jamesps@ebi.ac.uk>
2023-05-10 11:37:35 +0100

    Implement locks feature

--- manifests/db.pp
+++ manifests/db.pp
@@ -23,9 +23,17 @@
 #
 # @param db_file Absolute path of the dconf db file
 #
+# @param locks_dir Absolute path of the dconf db locks directory
+#
+# @param locks_file Absolute path of the dconf db locks file
+#
 # @param db_dir_mode File permissions for dconf db directory
 #
 # @param db_file_mode File permissions for dconf db file
+#
+# @param locks_dir_mode File permissions for dconf db locks directory
+#
+# @param locks_file_mode File permissions for dconf db locks file
 #
 # @param db_dir_purge Whether to purge the dconf db directory of unmanaged files
 #
@@ -38,8 +46,12 @@ define dconf::db (
   Stdlib::Absolutepath $base_dir = '/etc/dconf/db',
   Stdlib::Absolutepath $db_dir = "${base_dir}/${name}.d",
   Stdlib::Absolutepath $db_file = "${db_dir}/00-default",
+  Stdlib::Absolutepath $locks_dir = "${db_dir}/locks",
+  Stdlib::Absolutepath $locks_file = "${locks_dir}/00-default",
   String $db_dir_mode  = '0755',
   String $db_file_mode = '0644',
+  String $locks_dir_mode = '0755',
+  String $locks_file_mode = '0644',
   Boolean $db_dir_purge = true,
   Hash $inifile_defaults = { ensure => 'present', path => $db_file, notify => Exec['dconf_update'], require => File[$db_file], },
 ) {
@@ -54,6 +66,25 @@ define dconf::db (
     require => File[$db_dir],
   }
   if $settings {
-    inifile::create_ini_settings($settings,$inifile_defaults) 
+    inifile::create_ini_settings($settings,$inifile_defaults)
+  }
+  if $locks {
+    file { $locks_dir:
+      ensure => 'directory',
+      mode   => $locks_dir_mode,
+    }
+    concat { "db_${name}_locks":
+      path    => $locks_file,
+      mode    => $locks_file_mode,
+      order   => 'alpha',
+      require => File[$locks_dir],
+    }
+    $locks.each |$lock| {
+      concat::fragment { "db_${name}_locks_${lock}": 
+        target  => $locks_file,
+        content => "${lock}\n",
+        require => Concat["db_${name}_locks"],
+      }
+    }
   }
 }
