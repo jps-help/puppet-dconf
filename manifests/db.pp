1640f277e1df3614d80db719bb4b0c41c28e56c4
jamesps-ebi <83767764+jamesps-ebi@users.noreply.github.com>
2023-07-20 15:10:49 +0100

    Allow multiple db files under a single db (#10)
    * Allow management of multiple dconf db files under a single db.
    
    * Example - Managing multiple config files for the same db
    
    * Fix profile.pp example
    
    * Fix indentation
    
    * Fix double-quoted string
    
    * Module cleanup and add REFERENCE.md
    
    * Add parameter for locks filename
    
    * Fix documenation and bump module version metadata
    
    ---------
    
    Co-authored-by: James Paton-Smith <jpatonsmith@hotmail.com>

--- manifests/db.pp
+++ manifests/db.pp
@@ -4,7 +4,7 @@
 #   dconf::db { 'local':
 #     settings => {
 #       'system/proxy/http' => {
-#         'host' => '172.16.0.1',
+#         'host' => "'172.16.0.1'",
 #         'enabled' => 'true',
 #       },
 #       'org/gnome/desktop/background' => {
@@ -17,13 +17,34 @@
 #   dconf::db { 'local':
 #     settings => {
 #       'system/proxy/http' => {
-#         'host' => '172.16.0.1',
+#         'host' => "'172.16.0.1'",
 #         'enabled' => 'true',
 #       },
 #     locks => [
 #       'system/proxy/http/host',
 #       'system/proxy/http/enabled',
 #     ],
+#
+# @example Managing multiple config files for the same db
+#   dconf::db { 'system-proxy':
+#     db_dir      => '/etc/dconf/db/local.d',
+#     db_filename => 'system-proxy',
+#     settings    => {
+#       'system/proxy/http' => {
+#         'host'    => "'172.16.0.1'",
+#         'enabled' => 'true',
+#       },
+#     },
+#   }
+#   dconf::db { 'disable-microphone':
+#     db_dir      => '/etc/dconf/db/local.d',
+#     db_filename => 'disable-micrphone',
+#     settings    => {
+#       'org/gnome/desktop/privacy' => {
+#         'disable-microphone' => 'true',
+#       },
+#     },
+#   }
 #
 # @param settings Hash of dconf settings
 #
@@ -33,9 +54,13 @@
 #
 # @param db_dir Absolute path of the dconf db directory
 #
+# @param db_filename Name of the dconf db file
+#
 # @param db_file Absolute path of the dconf db file
 #
 # @param locks_dir Absolute path of the dconf db locks directory
+#
+# @param locks_filename Name of the dconf locks file
 #
 # @param locks_file Absolute path of the dconf db locks file
 #
@@ -55,15 +80,16 @@
 #
 # @param inifile_defaults Default parameters to parse to inifile provider
 #
-# @param
 define dconf::db (
   Optional[Hash] $settings = undef,
   Optional[Array] $locks = undef,
   Stdlib::Absolutepath $base_dir = '/etc/dconf/db',
   Stdlib::Absolutepath $db_dir = "${base_dir}/${name}.d",
-  Stdlib::Absolutepath $db_file = "${db_dir}/00-default",
+  String $db_filename = '00-default',
+  Stdlib::Absolutepath $db_file = "${db_dir}/${db_filename}",
   Stdlib::Absolutepath $locks_dir = "${db_dir}/locks",
-  Stdlib::Absolutepath $locks_file = "${locks_dir}/00-default",
+  String $locks_filename = '00-default',
+  Stdlib::Absolutepath $locks_file = "${locks_dir}/${locks_filename}",
   String $base_dir_mode = '0755',
   String $db_dir_mode  = '0755',
   String $db_file_mode = '0644',
@@ -73,14 +99,17 @@ define dconf::db (
   Enum['present','absent'] $ensure = 'present',
   Hash $inifile_defaults = { ensure => 'present', path => $db_file, notify => Exec['dconf_update'], require => File[$db_file], },
 ) {
-  ensure_resource(file, $base_dir, { ensure => 'directory',mode   => $base_dir_mode })
+  ensure_resource(file, $base_dir, {
+      ensure => 'directory',
+      mode   => $base_dir_mode,
+  })
   if $ensure == 'present' {
-    file { $db_dir:
-      ensure  => 'directory',
-      mode    => $db_dir_mode,
-      purge   => $purge,
-      recurse => $purge,
-    }
+    ensure_resource(file, $db_dir, {
+        ensure  => 'directory',
+        mode    => $db_dir_mode,
+        purge   => $purge,
+        recurse => $purge,
+    })
     file { $db_file:
       ensure  => 'file',
       mode    => $db_file_mode,
@@ -90,12 +119,12 @@ define dconf::db (
       inifile::create_ini_settings($settings,$inifile_defaults)
     }
     if $locks {
-      file { $locks_dir:
-        ensure  => 'directory',
-        mode    => $locks_dir_mode,
-        purge   => $purge,
-        recurse => $purge,
-      }
+      ensure_resource(file, $locks_dir, {
+          ensure  => 'directory',
+          mode    => $locks_dir_mode,
+          purge   => $purge,
+          recurse => $purge,
+      })
       concat { "db_${name}_locks":
         path    => $locks_file,
         mode    => $locks_file_mode,
